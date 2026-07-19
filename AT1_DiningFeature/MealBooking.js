cc0bc498ade49c4787fae8a73e5136dc14137c14
nathanielposanai21-IS <nathanielposanai21@gmail.com>
2026-07-19 19:45:29 +1000

    Declaring Private Fields and Constructor

--- AT1_DiningFeature/MealBooking.js
+++ AT1_DiningFeature/MealBooking.js
@@ -1,3 +1,18 @@
 class MealBooking {
+    constructor(studentID, studentName, mealDate, mealType, quantity, dietaryNote) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+        this.mealDate = mealDate;
+        this.mealType = mealType;
+        this.quantity = quantity;
+        this.dietaryNote = dietaryNote;
 
+        this.bookingStatus = 'Pending'; // Default status
+
+        this.mealPrices = {
+            'Breakfast': 10,
+            'Lunch': 15,
+            'Dinner': 20
+        };
+    }
 }
